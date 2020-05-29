793f66ec44efceef8fc676907ffa1ab184f4dae2
biji <heapoverflow@kakao.com>
2020-05-30 00:14:19 +0900

    test

--- lupcapd.h
+++ lupcapd.h
@@ -4,6 +4,22 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct{
+    uint16_t data_length;
+    lupcap_data data;
+}socket_data;
+
+typedef struct{
+    uint16_t type;
+    uint8_t ret;
+    uint16_t data_length;
+}lupcap_header;
+
+typedef struct{
+    lupcap_header header;
+    uint8_t body[1460]; 
+}lupcap_data;
+
 bool lupcap_close(pcap_t * handle);
 bool lupcap_findalldevs(uint16_t *data_length, uint8_t * data);
 bool lupcap_read(pcap_t * handle, uint16_t * data_length, uint8_t * data);
--- main.cpp
+++ main.cpp
@@ -8,56 +8,32 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <thread>
 #include "lupcapd.h"
 
-#define SEND_SIZE 1460
+using namespace std;
 
-int main(){
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    int client_fd;
-    struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
-    int len;
+#define SEND_SIZE 1460
+int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+void sig_handler(int signo){
+    close(server_fd);
+    exit(1);
+}
+
+void client_thread(int client_fd){
+    pcap_t * handle;
+    bool ret_check;
+    pcap_pkthdr * header;
+    const unsigned char * rep;
+
     char type[2];
     uint16_t data_length;
     uint8_t data[SEND_SIZE];
     uint8_t save_data[SEND_SIZE];
     uint8_t recv_data[SEND_SIZE] ={0,};
     char dev[10] = {0,};
-
-    memset(&server_addr, 0, sizeof(server_addr));
-    memset(&client_addr, 0, sizeof(client_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(2020);
-
-
-    if(server_fd == -1){
-        printf("[-] socket creation failed\n");
-        return 0;
-    }
-    if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
-        printf("[-] bind failed\n");
-        return 0;
-    }
-    if(listen(server_fd, 5) < 0){
-        printf("[-] listen failed\n");
-        return 0;
-    }
-    len = sizeof(client_addr);
-    
-    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&len);
-    if(client_fd < 0){
-        close(server_fd);
-        exit(1);
-    }
-
-    printf("[*] connection success\n");
-
-    pcap_t * handle;
-    bool ret_check;
-    pcap_pkthdr * header;
-    const unsigned char * rep;
     
     while(1){
         printf("[+] waiting...\n");
@@ -109,7 +85,51 @@ int main(){
             }
         }
         send(client_fd, data, SEND_SIZE, 0);
+        if(memcmp(type, "\x11\x12", sizeof(type)) == 0){
+            break;
+        }
    }
    close(client_fd);
-   close(server_fd);
+}
+
+int main(){
+    signal(SIGINT, sig_handler);
+
+    int client_fd;
+    struct sockaddr_in server_addr;
+    struct sockaddr_in client_addr;
+    int len;
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&client_addr, 0, sizeof(client_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(2020);
+
+
+    if(server_fd == -1){
+        printf("[-] socket creation failed\n");
+        return 0;
+    }
+    if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+        printf("[-] bind failed\n");
+        return 0;
+    }
+    if(listen(server_fd, 5) < 0){
+        printf("[-] listen failed\n");
+        return 0;
+    }
+    len = sizeof(client_addr);
+    
+
+    while(1){
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t *)&len);
+        if(client_fd < 0){
+            close(server_fd);
+            exit(1);
+        }
+        printf("[*] connection success\n");
+        thread c_th(client_thread, client_fd);
+        c_th.detach();
+    }
 }
